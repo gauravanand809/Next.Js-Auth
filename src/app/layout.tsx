71e83c0279a004849df50b4f5c621ed1fc570650
gaurav <gaurav.anand.che22@iitbhu.ac.in>
2025-02-01 13:14:46 +0530

    title corrected

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,9 @@ import { Inter } from "next/font/google";
 import SessionProviderWrapper from "../components/SessionProviderWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
-
+const metadata = {
+  title:"Business Analytics"  
+}
 export default function RootLayout({
   children,
 }: {
@@ -12,6 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>
+          {metadata.title}
+        </title>
+      </head>
       <body className={inter.className}>
         <SessionProviderWrapper>{children}</SessionProviderWrapper>
       </body>
